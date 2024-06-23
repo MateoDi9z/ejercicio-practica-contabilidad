ad823a118d5e256c0ff812fc8d4a23628bdf8c0d
MateoDi9z <dzmateozd@gmail.com>
2024-06-23 15:44:50 -0300

    init

---
+++ README.md
@@ -0,0 +1,27 @@
+# Sistema de contabilidad para practicar python
+
+Sistema para registrar salidas y entradas de dinero.
+
+> Modificar unicamente el archivo `main.py`
+
+## Consigna
+
+El archivo main contiene una lista de funcionalidades a implementar para mejorar el sistema de contabilidad.
+
+## Funciones a utilizar
+
+```py
+# Ejemplos
+
+## Crear registros
+crear_nuevo_ingreso("1", "Mateo", 100)
+crear_nuevo_egreso("1", "Mateo", 50)
+
+# Ver registros
+print(ingresos())
+print(gastos())
+
+# Borrar registros
+clear_ingresos()
+clear_gastos()
+```
---
+++ main.py
@@ -0,0 +1,45 @@
+def main():
+  """
+    ### Sistema de contabilidad
+  
+    #### Funcionalidades a desarrollar:
+      - Crear una entrada de dinero
+      - Crear una salida de dinero
+      - Ver los ingresos
+      - Ver los gastos separados por destino
+      - Calcular saldo de cuenta
+      - Ver los ingresos separados por inicial de nombre 
+      - Ver los gastos de un único destino
+  """
+
+  print("\nBienvenido a el sistema de contabilidad!")
+
+  while True:
+    print("\nElija una opción! \n")
+    print(" 1 - Crear entrada")
+    print(" 2 - Crear salida")
+
+    # TODO: Crea el resto de opciones aca :)
+
+    print(" 3 - Salir")
+    entrada = input("> ")
+    
+    print("")
+    # TODO: Definí una función para resolver cada opción
+    if entrada == "1":
+      print("Not implemented!")
+    elif entrada == "2":
+      print("Not implemented!")
+    elif entrada == "3":
+      print("Not implemented!")
+
+main()
+
+"""
+Tabla de contabilidad
+
+Gastos | Ingresos
+-----------------
+    10 | 5
+
+"""
